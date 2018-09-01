848d79cb2dbe27329300c9b05e69eaa4376095c8
Addison Wen <perhaps0660@gmail.com>
2018-09-01 22:32:16 +1000

    updated

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+const helmet = require('helmet')
+const morgan = require('morgan')
 const Joi = require('joi')
+const logger = require('./logger')
 const express = require('express')
 const app = express()
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+app.use(helmet())
+app.use(morgan('tiny'))
+
+// middleware
+app.use(logger)
 
 const courses = [
     {id: 1, name:'hahaha'},
---
+++ logger.js
@@ -0,0 +1,8 @@
+// middleware
+
+function log (req, res, next) {
+    console.log('logging')
+    next()
+}
+
+module.exports = log
